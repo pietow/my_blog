3ce6a5d07159f611c848e027521b21371733c569
piet <pprotassow@posteo.de>
2023-10-18 19:12:48 +0200

    forms added

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
 from .models import Post
 
 # Create your views here.
@@ -11,7 +13,17 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
     
-    
-    def get(self, request, *args, **kwargs):
-        # print('BLA', self.kwargs.get('pk'))
-        return super().get(request, *args, **kwargs)
+class BlogCreateView(CreateView): # new
+    model = Post
+    template_name = "post_new.html"
+    fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView): # new
+    model = Post
+    template_name = "post_edit.html"
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse("home")
