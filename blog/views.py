a3a4c0ffb651a6f67bd702d25b6709661cfccf72
piet <pprotassow@posteo.de>
2023-10-18 17:06:59 +0200

    working blog without Forms

---
+++ blog/views.py
@@ -0,0 +1,17 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+# Create your views here.
+
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+    
+    
+    def get(self, request, *args, **kwargs):
+        # print('BLA', self.kwargs.get('pk'))
+        return super().get(request, *args, **kwargs)
